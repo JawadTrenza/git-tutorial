730562318c1c30465929e0d5f23fdf443baf59a8
jawad <jawad@trenzasoft.com>
2021-01-27 18:31:54 +0600

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,4 +2,8 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("vs code is awesome...");
     }
+    greeting("jawad");
+    public static void greeting(String name){
+        System.out.println("Welcome"+ name);
+    }
 }
